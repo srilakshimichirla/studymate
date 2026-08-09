1f83563d5de8da2252e3284b1543739e42e7d299
Srilakshmi <nagasrilakshmi923@gmail.com>
2026-08-09 18:24:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://studymate-4.onrender.com'
+        'https://studymate-5.onrender.com'
     ],
     credentials: true
 }));
